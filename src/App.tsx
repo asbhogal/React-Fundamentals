faba733551d62d4e69d6ba428e21ebdda9afda18
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-05-03 17:45:48 +0100

    added button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 export default function App() {
   const items = ["London", "Paris", "New York", "Rome", "Berlin", "Moscow"];
   const handleSelectItem = (item: string) => {
     console.log(item);
-  }
+  };
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>
+        <span>Test</span>
+      </Alert>
+      <Button color="primary" onClick={() => console.log("button clicked")}>
+        Test
+      </Button>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+interface Props {
+  children: string;
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark";
+  onClick: () => void;
+}
+
+const Button = ({ children, color = "primary", onClick }: Props) => {
+  return (
+    <button className={`btn btn-${color}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
